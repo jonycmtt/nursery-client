b0adeee91228f1583707476e393d614a81ee1a96
Salman Rahaman Jony <jonyislamcmt@gmail.com>
2024-07-13 00:45:14 +0600

    [update] shopping cart length in cart button

--- src/pages/MainPage/ShoppingCart.tsx
+++ src/pages/MainPage/ShoppingCart.tsx
@@ -4,26 +4,28 @@ import ProductHeader from "./Product/ProductHeader";
 import { useForm } from "react-hook-form";
 import { useCreateOrderDataMutation } from "../../redux/features/cart/cartApi";
 import { toast } from "sonner";
+import { Link } from "react-router-dom";
 
 const ShoppingCart = () => {
-  const delay = (ms: number | undefined) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
-
   const { register, handleSubmit } = useForm();
   const cartData = useAppSelector((cart) => cart.cart.cart);
   const [checkout, setCheckout] = useState(false);
-  const [createOrder, { data, isLoading, isSuccess, isError, error }] =
+  const [createOrder, { isLoading, isSuccess, isError }] =
     useCreateOrderDataMutation();
 
-  console.log(cartData);
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("Order accepted!");
+    }
 
-  if (isSuccess) {
-    toast.success("Order accept!");
-  }
+    if (isError) {
+      toast.error("Something went wrong!");
+    }
+  }, [isSuccess, isError]);
 
   const totalCharge = cartData.reduce((acc, item) => {
     return acc + item.totalPrice;
-  }, 0);
+  }, 50);
   useEffect(() => {
     const handleBeforeUnload = (event) => {
       if (cartData.length > 0) {
@@ -65,196 +67,218 @@ const ShoppingCart = () => {
   return (
     <div>
       <ProductHeader title="Shopping Cart" />
-      <div className="my-16 max-w-6xl mx-auto ">
-        <table className="border w-full text-center">
-          <thead>
-            <tr>
-              <td className="border p-2">Image</td>
-              <td className="border p-2">Product Title</td>
-              <td className="border p-2">Category</td>
-              <td className="border p-2">Quantity</td>
-              <td className="border p-2">Unit Price</td>
-              <td className="border p-2">Total</td>
-            </tr>
-          </thead>
-          <tbody>
-            {cartData?.map((cart: any) => (
-              <tr key={cart._id}>
-                <td className="p-1 border flex justify-center items-center">
-                  <img className="w-12" src={cart.imageUrl} alt="" />
-                </td>
-                <td className="border p-1">{cart.title}</td>
-                <td className="border p-1">{cart.category}</td>
-                <td className="border p-1">{cart.quantity}</td>
-                <td className="border p-1">${cart.price}.00</td>
-                <td className="border p-1">${cart.totalPrice}.00</td>
+      {cartData.length ? (
+        <div className="my-16 max-w-6xl mx-auto ">
+          <table className="border w-full text-center">
+            <thead>
+              <tr>
+                <td className="border p-2">Image</td>
+                <td className="border p-2">Product Title</td>
+                <td className="border p-2">Category</td>
+                <td className="border p-2">Quantity</td>
+                <td className="border p-2">Unit Price</td>
+                <td className="border p-2">Total</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {cartData?.map((cart: any) => (
+                <tr key={cart._id}>
+                  <td className="p-1 border flex justify-center items-center">
+                    <img className="w-12" src={cart.imageUrl} alt="" />
+                  </td>
+                  <td className="border p-1">{cart.title}</td>
+                  <td className="border p-1">{cart.category}</td>
+                  <td className="border p-1">{cart.quantity}</td>
+                  <td className="border p-1">${cart.price}.00</td>
+                  <td className="border p-1">${cart.totalPrice}.00</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-        <div className="mt-20">
-          <h2 className="text-2xl font-semibold">
-            What would you like to do next?
-          </h2>
-          <p className="text-slate-500">
-            Choose if you have a discount code or reward points you want to use
-            or would like to estimate your delivery cost.
-          </p>
-          <div className="max-w-xl ms-auto border my-6">
-            <div className="flex">
-              <td className="p-5 border w-full text-end">Delivery Charge </td>
-              <td className="p-5 border w-full text-end">$50.00</td>
-            </div>
-            <div className="flex">
-              <td className="p-5 border w-full text-end">Total Charge</td>
-              <td className="p-5 border w-full text-end">
-                ${totalCharge + 50}.00
-              </td>
+          <div className="mt-20">
+            <h2 className="text-2xl font-semibold">
+              What would you like to do next?
+            </h2>
+            <p className="text-slate-500">
+              Choose if you have a discount code or reward points you want to
+              use or would like to estimate your delivery cost.
+            </p>
+            <div className="max-w-xl ms-auto border my-6">
+              <div className="flex">
+                <td className="p-5 border w-full text-end">Delivery Charge </td>
+                <td className="p-5 border w-full text-end">$50.00</td>
+              </div>
+              <div className="flex">
+                <td className="p-5 border w-full text-end">Total Charge</td>
+                <td className="p-5 border w-full text-end">
+                  ${totalCharge}.00
+                </td>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="text-end">
-          <button onClick={() => setCheckout(true)} className="btn btn-neutral">
-            {checkout ? "Processing" : "Checkout"}
-          </button>
-        </div>
+          <div className="text-end">
+            <button
+              onClick={() => setCheckout(true)}
+              className="btn btn-neutral"
+            >
+              {checkout ? (
+                <div className="flex items-center gap-2">
+                  <span className="loading loading-ring loading-md"></span>
+                  <span> Processing...</span>
+                </div>
+              ) : (
+                "Checkout"
+              )}
+            </button>
+          </div>
 
-        {/* checkout form */}
-        {checkout && (
-          <div className="my-24">
-            <h1 className="text-2xl font-semibold text-center">
-              Checkout Form
-            </h1>
-            {/* <div className="my-12 flex justify-between items-center">
+          {/* checkout form */}
+          {checkout && (
+            <div className="my-24">
+              <h1 className="text-2xl font-semibold text-center">
+                Checkout Form
+              </h1>
+              {/* <div className="my-12 flex justify-between items-center">
             <h2>Your Personal Details</h2>
             <h2>Your Address</h2>
           </div> */}
-            <form onSubmit={handleSubmit(onOrderSubmit)} className=" my-12">
-              <div className="flex justify-center flex-col lg:flex-row gap-12">
-                <div className="w-full">
-                  <h2 className="text-xl font-semibold border-b pb-2">
-                    Your Personal Details
-                  </h2>
-                  <div className="flex flex-col gap-y-6 my-6">
-                    <div className="flex  flex-col gap-y-2">
-                      <label className="text-slate-600" htmlFor="name">
-                        Your Full Name :
-                      </label>
-                      <input
-                        {...register("name")}
-                        id="name"
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="Your Name"
-                      />
+              <form onSubmit={handleSubmit(onOrderSubmit)} className=" my-12">
+                <div className="flex justify-center flex-col lg:flex-row gap-12">
+                  <div className="w-full">
+                    <h2 className="text-xl font-semibold border-b pb-2">
+                      Your Personal Details
+                    </h2>
+                    <div className="flex flex-col gap-y-6 my-6">
+                      <div className="flex  flex-col gap-y-2">
+                        <label className="text-slate-600" htmlFor="name">
+                          Your Full Name :
+                        </label>
+                        <input
+                          {...register("name")}
+                          id="name"
+                          className="input input-bordered"
+                          type="text"
+                          placeholder="Your Name"
+                        />
+                      </div>
+                      <div className="flex  flex-col gap-y-2">
+                        <label className="text-slate-600" htmlFor="email">
+                          Your Email :
+                        </label>
+                        <input
+                          {...register("email")}
+                          id="email"
+                          className="input input-bordered"
+                          type="email"
+                          placeholder="Your Email"
+                        />
+                      </div>
+                      <div className="flex  flex-col gap-y-2">
+                        <label className="text-slate-600" htmlFor="phone">
+                          Your Phone Number :
+                        </label>
+                        <input
+                          {...register("phone")}
+                          id="phone"
+                          className="input input-bordered"
+                          type="text"
+                          placeholder="Your Phone No"
+                        />
+                      </div>
                     </div>
-                    <div className="flex  flex-col gap-y-2">
-                      <label className="text-slate-600" htmlFor="email">
-                        Your Email :
-                      </label>
-                      <input
-                        {...register("email")}
-                        id="email"
-                        className="input input-bordered"
-                        type="email"
-                        placeholder="Your Email"
-                      />
-                    </div>
-                    <div className="flex  flex-col gap-y-2">
-                      <label className="text-slate-600" htmlFor="phone">
-                        Your Phone Number :
-                      </label>
-                      <input
-                        {...register("phone")}
-                        id="phone"
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="Your Phone No"
-                      />
+                  </div>
+                  <div className="w-full">
+                    <h2 className="text-xl font-semibold border-b pb-2">
+                      Your Address
+                    </h2>
+                    <div className="flex flex-col gap-y-6 my-6">
+                      <div className="flex  flex-col gap-y-2">
+                        <label className="text-slate-600" htmlFor="Company">
+                          Company :
+                        </label>
+                        <input
+                          {...register("company")}
+                          id="Company"
+                          className="input input-bordered"
+                          type="text"
+                          placeholder="Company"
+                        />
+                      </div>
+                      <div className="flex  flex-col gap-y-2">
+                        <label className="text-slate-600" htmlFor="Address1">
+                          Address 1 :
+                        </label>
+                        <input
+                          {...register("address")}
+                          id="Address"
+                          className="input input-bordered"
+                          type="text"
+                          placeholder="Address 1"
+                        />
+                      </div>
+                      <div className="flex  flex-col gap-y-2">
+                        <label className="text-slate-600" htmlFor="City">
+                          City :
+                        </label>
+                        <input
+                          {...register("city")}
+                          id="City"
+                          className="input input-bordered"
+                          type="text"
+                          placeholder="City"
+                        />
+                      </div>
+                      <div className="flex  flex-col gap-y-2">
+                        <label className="text-slate-600" htmlFor="PostCode">
+                          Post Code :
+                        </label>
+                        <input
+                          {...register("postCode")}
+                          id="PostCode"
+                          className="input input-bordered"
+                          type="text"
+                          placeholder="Post Code"
+                        />
+                      </div>
+                      <div className="flex  flex-col gap-y-2">
+                        <label className="text-slate-600" htmlFor="Country">
+                          Country :
+                        </label>
+                        <input
+                          {...register("country")}
+                          id="Country"
+                          className="input input-bordered"
+                          type="text"
+                          placeholder="Country"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="w-full">
-                  <h2 className="text-xl font-semibold border-b pb-2">
-                    Your Address
-                  </h2>
-                  <div className="flex flex-col gap-y-6 my-6">
-                    <div className="flex  flex-col gap-y-2">
-                      <label className="text-slate-600" htmlFor="Company">
-                        Company :
-                      </label>
-                      <input
-                        {...register("company")}
-                        id="Company"
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="Company"
-                      />
-                    </div>
-                    <div className="flex  flex-col gap-y-2">
-                      <label className="text-slate-600" htmlFor="Address1">
-                        Address 1 :
-                      </label>
-                      <input
-                        {...register("address")}
-                        id="Address"
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="Address 1"
-                      />
-                    </div>
-                    <div className="flex  flex-col gap-y-2">
-                      <label className="text-slate-600" htmlFor="City">
-                        City :
-                      </label>
-                      <input
-                        {...register("city")}
-                        id="City"
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="City"
-                      />
-                    </div>
-                    <div className="flex  flex-col gap-y-2">
-                      <label className="text-slate-600" htmlFor="PostCode">
-                        Post Code :
-                      </label>
-                      <input
-                        {...register("postCode")}
-                        id="PostCode"
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="Post Code"
-                      />
-                    </div>
-                    <div className="flex  flex-col gap-y-2">
-                      <label className="text-slate-600" htmlFor="Country">
-                        Country :
-                      </label>
-                      <input
-                        {...register("country")}
-                        id="Country"
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="Country"
-                      />
-                    </div>
-                  </div>
+                <div className="text-center mt-12">
+                  {isLoading ? (
+                    <span className="loading loading-ring loading-lg"></span>
+                  ) : (
+                    <button className="btn btn-neutral">Confirm Order</button>
+                  )}
                 </div>
-              </div>
-              <div className="text-center mt-12">
-                {isLoading ? (
-                  <span className="loading loading-ring loading-lg"></span>
-                ) : (
-                  <button className="btn btn-neutral">Confirm Order</button>
-                )}
-              </div>
-            </form>
-          </div>
-        )}
-      </div>
+              </form>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="min-h-96 flex gap-y-10 flex-col justify-center items-center">
+          <img
+            src="https://i.ibb.co/Zx3qbVS/3bd05447340145-587755b7635a0.gif"
+            alt=""
+          />
+          <Link to="/products">
+            <button className="btn btn-neutral">Go To Shopping</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/MainPage/TopHeader.tsx
+++ src/pages/MainPage/TopHeader.tsx
@@ -2,14 +2,13 @@ import { Link, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useAppDispatch } from "../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { searchInput } from "../../redux/features/product/productSlice";
 
 const TopHeader = () => {
   const dispatch = useAppDispatch();
   const [openNav, setOpenNav] = useState(false);
   const { register, handleSubmit } = useForm();
-  // const categoryData = data;
   const navigate = useNavigate();
   const onSearchSubmit = (data) => {
     const searchInfo = {
@@ -18,6 +17,8 @@ const TopHeader = () => {
     dispatch(searchInput(searchInfo));
     navigate(`/products/search`);
   };
+
+  const cartItemSelector = useAppSelector((item) => item.cart.cart);
 
   return (
     <div className="bg-black bg-opacity-85  text-white relative">
@@ -107,7 +108,7 @@ const TopHeader = () => {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-6 cursor-pointer"
+            className="size-8 cursor-pointer"
           >
             <path
               strokeLinecap="round"
@@ -117,20 +118,25 @@ const TopHeader = () => {
           </svg>
 
           <Link to="/shoppingCart">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6 cursor-pointer"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-              />
-            </svg>
+            <div className="relative">
+              <span className="size-5 bg-green-600 rounded-full block text-sm text-center font-semibold absolute -top-3 right-0">
+                {cartItemSelector && cartItemSelector.length}
+              </span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-8 cursor-pointer"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                />
+              </svg>
+            </div>
           </Link>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -138,7 +144,7 @@ const TopHeader = () => {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-6 cursor-pointer"
+            className="size-8 cursor-pointer"
           >
             <path
               strokeLinecap="round"
