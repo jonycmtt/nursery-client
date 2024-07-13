9dbb7ae2514cfb83f83cb9632503d440bed4a186
Salman Rahaman Jony <jonyislamcmt@gmail.com>
2024-07-13 21:06:39 +0600

    [update] order details

--- src/pages/Deshbaord/Order.tsx
+++ src/pages/Deshbaord/Order.tsx
@@ -1,7 +1,76 @@
+import { Skeleton } from "antd";
+import { useGetOrderDataQuery } from "../../redux/features/cart/cartApi";
+import { TOrder, TOrderProductItemType } from "../../types/productType";
+
 const Order = () => {
+  const { data, isLoading } = useGetOrderDataQuery(undefined);
+
+  const OrderData = data?.data;
+
+  console.log(data?.data);
   return (
     <div>
-      <h2>This is Order component</h2>
+      <h2 className="text-xl sm:text-4xl font-semibold ">Order List</h2>
+      <div className="overflow-x-auto my-12 ">
+        {isLoading ? (
+          <>
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+          </>
+        ) : (
+          <>
+            {OrderData.length ? (
+              <table className="table w-full text-gray-500">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th>SL No</th>
+                    <th>ProductId</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th>Cash on delivery</th>
+                    <th>Hand Case</th>
+                    <th>Total Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {OrderData?.map((item: TOrder, index: number) => (
+                    <tr key={item._id}>
+                      <td>{index + 1}</td>
+                      <td>
+                        {item.productItem.map(
+                          (item: TOrderProductItemType, ind: number) => (
+                            <div className="flex flex-col gap-y-01">
+                              <span>
+                                {ind + 1}: {item._id}
+                              </span>
+                            </div>
+                          )
+                        )}
+                      </td>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.phoneNumber}</td>
+                      <td>{item.CashOnDelivery ? "true" : "false"}</td>
+                      <td>{item.StripePayment ? "true" : "false"}</td>
+                      <td>{item.TotalPrice}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <div className="w-full flex justify-center items-center h-80">
+                <p className="text-center text-xl text-slate-400">
+                  Not Order Found!
+                </p>
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/types/productType.ts
+++ src/types/productType.ts
@@ -15,17 +15,31 @@ export type Category = {
   description?: string;
 };
 
+export type TOrderProductItemType = {
+  _id: string;
+  quantity: number;
+  title: string;
+  category: string;
+  imageUrl: string;
+};
+
 export type TOrder = {
+  productItem: [];
+  _id: string;
   cashOnDelivery: boolean;
+  CashOnDelivery?: boolean;
   stripe: boolean;
+  StripePayment?: boolean;
   name: string;
   email: string;
   phone: string;
+  phoneNumber?: string;
   company: string;
   address: string;
   city: string;
   postCode: number;
   country: string;
+  TotalPrice?: number;
 };
 
 export type TCartItem = {
